5f6297cc49391ed0a9c8193c7801608a7b40c5db
JesseMckinzie <jessemckinzie145@gmail.com>
2024-07-11 07:27:09 -0600

    Remove unused methods

--- src/cpp/utilities/sequence.h
+++ src/cpp/utilities/sequence.h
@@ -7,13 +7,10 @@ class Seq
 {
     private:
         long start_index_, stop_index_, step_;
-        bool is_valid_dimension_ = true;
     public:
         inline Seq(const long start, const long  stop, const long  step=1):start_index_(start), stop_index_(stop), step_(step){} 
         inline long Start()  const  {return start_index_;}
         inline long Stop()  const {return stop_index_;}
         inline long Step()  const {return step_;}
-        inline bool IsValidDimension() const {return is_valid_dimension_;}
-        inline void SetValidDimension(bool valid) {is_valid_dimension_ = valid;}
 };
 } //ns bfiocpp
