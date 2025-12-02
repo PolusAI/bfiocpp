f3cc8d2137b8f550f1b3fda637cf68f3095b1c2a
Sameeul B Samee <sameeul@gmail.com>
2025-12-02 13:14:17 -0500

    restore to main

--- setup.py
+++ setup.py
@@ -77,11 +77,6 @@ class CMakeBuild(build_ext):
         subprocess.check_call(
             ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env
         )
-        if platform.system() == "Linux":
-            result = subprocess.call(["sed", "-i", r"s/^#ifdef __has_builtin$/#if defined(__has_builtin) \&\& defined(__clang__)/", "_deps/tensorstore-src/tensorstore/internal/type_traits.h"],
-                                     cwd=self.build_temp, env=env)
-            if result != 0:
-                print("--------------- TensorStore patch failed!")
         subprocess.check_call(
             ["cmake", "--build", "."] + build_args, cwd=self.build_temp
         )
