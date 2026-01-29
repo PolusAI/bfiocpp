0cc45ac854daf1c33c356843d8cc7c9987a86cb4
Sameeul B Samee <sameeul@gmail.com>
2026-01-29 09:50:08 -0500

    WIP 3

--- ci-utils/install_prereq_linux.sh
+++ ci-utils/install_prereq_linux.sh
@@ -39,7 +39,7 @@ cd libjpeg-turbo-3.1.0
 mkdir build_man
 cd build_man
 cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DENABLE_STATIC=FALSE -DCMAKE_BUILD_TYPE=Release ..
-sudo make install -j4
+make install -j4
 cd ../../
 
 curl -L  https://github.com/glennrp/libpng/archive/refs/tags/v1.6.53.zip -o v1.6.53.zip
